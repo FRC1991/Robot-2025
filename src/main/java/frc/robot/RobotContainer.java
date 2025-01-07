3b8b043f30d9040289efd2462507eb3068bedc09
Emmet Spaeth <emmet.spaeth@gmail.com>
2025-01-07 13:36:51 -0500

    finalizing swerve code for testing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,21 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.subsystems.Swerve;
 
 public class RobotContainer {
+
+  private Swerve m_swerve = Swerve.getInstance();
+
   public RobotContainer() {
+
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    m_swerve.setDefaultCommand(new RunCommand(() -> m_swerve.update(), m_swerve));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
