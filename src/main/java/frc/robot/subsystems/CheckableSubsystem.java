1cb84b2ed5e2fd827da345f4524ce1535c9ba975
Emmet Spaeth <emmet.spaeth@gmail.com>
2025-01-17 19:41:02 -0500

    Reordering methods for clarity

--- src/main/java/frc/robot/subsystems/CheckableSubsystem.java
+++ src/main/java/frc/robot/subsystems/CheckableSubsystem.java
@@ -8,16 +8,16 @@ public interface CheckableSubsystem {
   void stop();
 
   /*
-   * This check should ensure that any data about a subsystem
-   * that can be determined in software is returning an acceptable
-   * output.
-   */
-  boolean checkSubsystem();
-
-  /*
    * Initialized should always represent if the constructor has run properly.
    * It can also have subsystem specific requirements, like a turret being aligned
    * to dead center before a match or a pivot needing to be level before a match.
    */
   boolean getInitialized();
+
+  /*
+   * This check should ensure that any data about a subsystem
+   * that can be determined in software is returning an acceptable
+   * output.
+   */
+  boolean checkSubsystem();
 }
